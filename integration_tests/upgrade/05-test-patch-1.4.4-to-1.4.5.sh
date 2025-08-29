aafa7f7feecdfc1f4a377fccb468269c7b42b83a
Renata Toktar <renata.toktar@dsr-corporation.com>
2025-08-29 07:41:33 +0300

    update test

--- integration_tests/upgrade/05-test-patch-1.4.4-to-1.4.5.sh
+++ integration_tests/upgrade/05-test-patch-1.4.4-to-1.4.5.sh
@@ -3,17 +3,17 @@
 set -euo pipefail
 source integration_tests/cli/common.sh
 
-binary_version="v1.4.4"
+binary_version_old="v1.4.4"
 binary_version_new="v1.4.5-0.dev.1"
 node_count=4
 
-wget -O dcld_old "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/$binary_version/dcld"
-chmod ugo+x dcld_old
-wget -O dcld_new "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/$binary_version_new/dcld"
+wget -O dcld_$binary_version_neww "https://github.com/zigbee-alliance/distributed-compliance-ledger/releases/download/$binary_version_new/dcld"
 chmod ugo+x dcld_new
 
-DCLD_BIN_OLD="./dcld_old"
-DCLD_BIN_NEW="./dcld_new"  # Path to locally built dcld v1.4.5
+
+DCLD_BIN_OLD="./dcld_$binary_version_old"
+DCLD_BIN_NEW="./dcld_$binary_version_new"  # Path to locally built dcld v1.4.5
+echo $($DCLD_BIN_NEW version)
 
 check_pool_accepts_tx() {
   # Generate random test data for transaction
@@ -105,6 +105,7 @@ test_divider
 echo "Rollback and upgrade all validator nodes to 1.4.5"
 for i in $(seq 0 $((node_count-1))); do
   name="node$i"
+  echo $($DCLD_BIN_NEW version)
   result=$(docker cp $DCLD_BIN_NEW $name:/var/lib/dcl/.dcl/cosmovisor/current/bin/)
   docker stop $name
   result=$($DCLD_BIN_NEW rollback --hard --home ./.localnet/$name)
